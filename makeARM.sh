dbb80ad289a27cf770d7d4f1a434b76b75c28ad6
Szymon-PC <wachim17@gmail.com>
2023-09-14 00:39:35 +0200

    /usr/lib/gcc-cross/arm-linux-gnueabihf/10/../../../../arm-linux-gnueabihf/bin/ld: cannot find -lsqlite3

--- makeARM.sh
+++ makeARM.sh
@@ -4,5 +4,5 @@ mkdir -p /build/Release
 mkdir -p /build/Debug
 
 cd build/Release
-cmake ../..
+cmake ../.. -D CMAKE_C_COMPILER=arm-linux-gnueabihf-gcc -D CMAKE_CXX_COMPILER=arm-linux-gnueabihf-g++ -DRELTYP="ARM"
 make
