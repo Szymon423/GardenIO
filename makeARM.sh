8609b92444b747aa9107a16e6452290714e134f2
Szymon-PC <wachim17@gmail.com>
2023-09-14 18:59:45 +0200

    compiled well

--- makeARM.sh
+++ makeARM.sh
@@ -4,5 +4,5 @@ mkdir -p /build/Release
 mkdir -p /build/Debug
 
 cd build/Release
-cmake ../.. -D CMAKE_C_COMPILER=arm-linux-gnueabihf-gcc -D CMAKE_CXX_COMPILER=arm-linux-gnueabihf-g++ -DRELTYP="ARM"
+cmake ../.. -D CMAKE_C_COMPILER=arm-linux-gnueabihf-gcc -D CMAKE_CXX_COMPILER=arm-linux-gnueabihf-g++ -DRELTYP="ARM" -DSQLITECPP_INTERNAL_SQLITE=ON
 make
