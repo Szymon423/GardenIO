acad0b8d6a5ef6ddd837b518a2ca8d4ff1781d5c
Szymon423 <wachim17@gmail.com>
2023-10-30 23:04:07 +0100

    intro to modbus server

--- protocols/modbus_dir/include/modbus_wrapper.hpp
+++ protocols/modbus_dir/include/modbus_wrapper.hpp
@@ -61,3 +61,51 @@ public:
     void SetSignal(ModbusOrder order);
 };
 
+
+class ModbusServer
+{
+private:
+    std::mutex mtx;
+    modbus_t* mb;
+    // time_t connectionInterval;
+    // std::string ip;
+    int port;
+    // time_t interval;
+    // time_t lastReadTime;
+    int returnedValue;
+    bool keepWorking;
+
+    uint8_t coils[MAX_COILS_NUMBER];
+    uint8_t inputs[MAX_INPUTS_NUMBER];
+    uint16_t holdingRegisters[MAX_HOLDING_REGISTERS_NUMBER];
+    uint16_t inputRegisters[MAX_INPUT_REGISTERS_NUMBER];
+
+    std::vector<ModbusSignal> modbusSignals;
+    std::vector<RegistersSet> continiousRegions_coils;
+    std::vector<RegistersSet> continiousRegions_inputs;
+    std::vector<RegistersSet> continiousRegions_holdingRegisters;
+    std::vector<RegistersSet> continiousRegions_inputRegisters;
+
+    std::vector<ModbusOrder> orders;
+
+    // void CreateContiniousRegistersSet();
+    // void ReadCoils();
+    // void ReadInputs();
+    // void ReadHoldingRegisters();
+    // void ReadInputRegisters();
+    void InterpreteRegisters();
+    void TranslateRegistersToValue(uint16_t* ptr_registers, ModbusSignal* ptr_signal);
+    void TranslateRegistersToValue(uint8_t* ptr_registers, ModbusSignal* ptr_signal);
+    // void ExecuteOrders();
+    uint16_t* TranslateValueToRegisters(ModbusSignal& signal, ModbusValue orderValue);
+
+
+public:
+    ModbusClient() = default;
+    ~ModbusClient();
+    // void SetConnectionParams(std::string newIp, int newPort);
+    // void SetConnectionInterval(time_t intervalTime);
+    // void SetSignalsDefinitions(std::vector<ModbusSignal> inputSignalsDefinitions);
+    // void RunInLoop();
+    // void SetSignal(ModbusOrder order);
+};
